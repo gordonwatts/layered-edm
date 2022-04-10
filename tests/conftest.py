9f1be4de4764d4aaf5442fcbd186a51aac944ee0
Gordon Watts <gordon@gordonandpaula.net>
2022-04-10 09:46:18 -0400

    awk registration function improvements (#38)
    * Registration Callback before bug fix
    
    * Fix flake8 error
    
    * Use the behavior mock in other tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import ast
 from typing import Union
 import sys
 
+import pytest
+
 # Hide ast.unparse
 if sys.version_info < (3, 9):
 
@@ -16,3 +18,24 @@ else:
         if isinstance(node, str):
             return node
         return ast.unparse(node)
+
+
+@pytest.fixture
+def ak_behavior(mocker):
+    "Patch the awkward behavior dictionary in `util_types`"
+    the_dict = {}
+    mocker.patch("layered_edm.util_types.ak.behavior", the_dict)
+    return the_dict
+
+
+@pytest.fixture
+def simple_ds():
+    import awkward as ak
+
+    return ak.Array(
+        [
+            [{"x": 1}, {"x": 2}, {"x": 3}],
+            [],
+            [{"x": 4}, {"x": 5}],
+        ]
+    )
